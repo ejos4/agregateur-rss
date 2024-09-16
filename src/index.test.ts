d6541d9dd0362092a3bb42bcd6783368ade84af7
ejos4 <erwanjos448@gmail.com>
2024-09-16 18:21:17 +0200

    Initial commit

---
+++ src/index.test.ts
@@ -0,0 +1,10 @@
+const {expect, test, describe} = require("@jest/globals");
+
+describe("The RSS Feed", () => {
+
+    test.todo("is getting data correctly");
+
+    test.todo("can be tested for required attributes");
+    
+    test.todo("returns articles");
+})
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+function getRSSFeed(RSSURL:string) {
+    return null;
+}
+
+function validateRSSFeed(RSSFedd:XMLDocument) {
+    return null;
+}
+
+function getArticlesFromRSSFeed(RSSFedd:XMLDocument) {
+    return null;
+}
